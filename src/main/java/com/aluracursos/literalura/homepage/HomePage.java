d484471fc4972fd5f1aae27fe6104fc8ae7e4984
carlapasadi@gmail.com <carlapasandi@hotmail.com>
2025-06-12 15:26:16 -0300

    Mejora en la compaginación de los autores / método propio para búsqueda y actualización de base de datos.

--- src/main/java/com/aluracursos/literalura/homepage/HomePage.java
+++ src/main/java/com/aluracursos/literalura/homepage/HomePage.java
@@ -8,6 +8,7 @@ import com.aluracursos.literalura.service.ConvertsData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
@@ -18,7 +19,7 @@ public class HomePage {
     private Scanner keyboard = new Scanner(System.in);
     private APIConsumption consumingAPI = new APIConsumption();
     private ConvertsData converter = new ConvertsData();
-    private final String URL_BASE= "https://gutendex.com/books/?";
+    private final String URL_BASE = "https://gutendex.com/books/?";
     private final String URL_SEARCH = "search=";
     private final String URL_START_YEAR = "author_year_start=";
     private final String URL_END_YEAR = "&author_year_end=";
@@ -28,9 +29,9 @@ public class HomePage {
     @Autowired
     private IAuthorRepository authorRepository;
 
-    public void showMenu(){
+    public void showMenu() {
         var exit = -1;
-        while(exit != 0) {
+        while (exit != 0) {
             System.out.println("""
                     \n¡Bienvenido a tu biblioteca virtual!\nIngresa una opción:\n
                     [1] Buscar libro por título.
@@ -44,7 +45,7 @@ public class HomePage {
             exit = keyboard.nextInt();
             keyboard.nextLine();
 
-            switch (exit){
+            switch (exit) {
                 case 1 -> findBookByTitle();
                 case 2 -> registeredBook();
                 case 3 -> registeredAuthors();
@@ -56,25 +57,57 @@ public class HomePage {
         }
     }
 
-    public GeneralData ConsumingAPI(String urlSearch, String userInput){
-        var json = consumingAPI.getData(URL_BASE + urlSearch + userInput.replace(" ","%20"));
+    public GeneralData ConsumingAPI(String urlSearch, String userInput) {
+        var json = consumingAPI.getData(URL_BASE + urlSearch + userInput.replace(" ", "%20"));
         var data = converter.getData(json, GeneralData.class);
 
         return data;
     }
 
-    public GeneralData ConsumingAPI(String urlStartYear, Integer userInput1, String urlEndYear, Integer userInput2){
+    public GeneralData ConsumingAPI(String urlStartYear, Integer userInput1, String urlEndYear, Integer userInput2) {
         var json = consumingAPI.getData(URL_BASE + urlStartYear + userInput1 + urlEndYear + userInput2);
         var data = converter.getData(json, GeneralData.class);
 
         return data;
     }
 
-    public GeneralData ConsumingAPI(String urlBase, String urlLanguages, String userInput){
+    public GeneralData ConsumingAPI(String urlBase, String urlLanguages, String userInput) {
         var json = consumingAPI.getData(urlBase + urlLanguages + userInput.toLowerCase());
         var data = converter.getData(json, GeneralData.class);
 
         return data;
+    }
+
+    public List<Author> saveAuthor(GeneralData data, Integer limitList) {
+        var authorFound = new ArrayList<Author>();
+
+        // Siempre verifica primero que las listas no estén vacías o null.
+        if (data.booksList() != null && !data.booksList().isEmpty()) {
+            List<BookData> bookDataList = data.booksList().stream()
+                    .limit(limitList)
+                    .collect(Collectors.toList());
+
+            for (BookData bookAPI : bookDataList) {
+                if (bookAPI.authorList() != null && !bookAPI.authorList().isEmpty()) {
+                    AuthorData authorAPI = bookAPI.authorList().get(0);
+
+                    // Verifica si ya existe en la base de datos o hay que agregarlo.
+                    Optional<Author> existingAuthor = authorRepository.findByCompleteName(authorAPI.completeName());
+
+                    Author authorReferent = null;
+                    if (existingAuthor.isPresent()) {
+                        authorReferent = existingAuthor.get();
+                        authorFound.add(authorReferent);
+                    } else {
+                        authorReferent = new Author(authorAPI);
+                        authorRepository.save(authorReferent); // Guarda al author
+                        authorFound.add(authorReferent);
+                    }
+                }
+            }
+        }
+
+        return authorFound;
     }
 
     private void findBookByTitle() {
@@ -141,31 +174,9 @@ public class HomePage {
 
         GeneralData data = ConsumingAPI(URL_START_YEAR, startYear, URL_END_YEAR, endYear);
 
-        // Tomará los primeros 5 resultados.
-        List<BookData> theData = data.booksList().stream()
-                .limit(5)
-                .collect(Collectors.toList());
+        System.out.println("Los autores seleccionados son:\n");
 
-        System.out.println("Encontramos los siguientes autores:\n");
-
-        for (BookData bookAPI : theData){
-            if(bookAPI.authorList() != null && !bookAPI.authorList().isEmpty()){
-                AuthorData authorAPI = bookAPI.authorList().get(0);
-
-                // Verifica si ya existe en la base de datos o hay que agregarlo.
-                Author authorReferent = null;
-                Optional<Author> existingAuthor = authorRepository.findByCompleteName(authorAPI.completeName());
-
-                if (existingAuthor.isPresent()) {
-                    authorReferent = existingAuthor.get();
-                    System.out.println(authorReferent);
-                } else {
-                    authorReferent = new Author(authorAPI);
-                    authorRepository.save(authorReferent);
-                    System.out.println(authorReferent);
-                }
-            }
-        }
+        saveAuthor(data, 5).forEach(System.out::println);
     }
 
     private void registeredBookByLenguage() {
